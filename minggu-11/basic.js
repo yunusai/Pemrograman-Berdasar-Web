473e1b605e89ec3f464526f0bfd886a7433e7e9d
yunusai <111202214341@mhs.dinus.ac.id>
2022-11-30 19:32:49 +0700

    Minggu 11

---
+++ minggu-11/basic.js
@@ -0,0 +1,10 @@
+//For untuk bilangan menurun
+for (var i = 10; i > 0; i++) {
+    document.write("<p>Perulangan ke-" + i + "</p>")
+}
+//Nested For
+for (var i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+        document.write("<p>Perulangan ke " + i + "," + j + "</p>");
+    }
+}
